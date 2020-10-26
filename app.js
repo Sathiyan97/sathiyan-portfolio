7231b301df6a75e349647e13c83b7d1ce9dd4a45
sathiyan.A <saththiyan97@gmail.com>
2020-10-26 18:55:52 +0530

    Added a Procfile.

--- app.js
+++ app.js
@@ -125,5 +125,5 @@ if(port==null||port==""){port=3000;}
 
 
 app.listen(port, function() {
-  console.log("Server has started on port successfully");
+  console.log("Server has started on port successfully.");
 });
