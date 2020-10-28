ddd9fdb3017df8342245b859c4641fa22b4ee4e5
sathiyan.A <saththiyan97@gmail.com>
2020-10-29 00:54:03 +0530

    Add

--- app.js
+++ app.js
@@ -71,6 +71,51 @@ app.post("/compose",function(req,res){
 //   console.log(req.body)
 // });
 
+// app.post('/send', (req, res) => {
+//   const output = `
+//     <p>You have a new contact request</p>
+//     <h3>Contact Details</h3>
+//     <ul>
+//       <li>Name: ${req.body.name}</li>
+//       <li>Company: ${req.body.company}</li>
+//       <li>Email: ${req.body.email}</li>
+//       <li>Phone: ${req.body.phone}</li>
+//     </ul>
+//     <h3>Message</h3>
+//     <p>${req.body.message}</p>
+//   `;
+
+//  // create reusable transporter object using the default SMTP transport
+//  let transporter = nodemailer.createTransport({
+//   host: 'smtp.gmail.com',
+//   port: 587,
+//   secure: false, // true for 465, false for other ports
+//   auth: {
+//       user: 'portfoliosthy2020@gmail.com', // generated ethereal user
+//       pass: 'Portfolio2020'  // generated ethereal password
+//   }
+// });
+//   // setup email data with unicode symbols
+//   let mailOptions = {
+//       from: '"Nodemailer Contact" <portfoliosthy2020@gmail.com>', // sender address
+//       to: 'saththiyan97@gmail.com', // list of receivers
+//       subject: 'Node Contact Request', // Subject line
+//       text: 'Hello world?', // plain text body
+//       html: output // html body
+//   };
+
+//   // send mail with defined transport object
+//   transporter.sendMail(mailOptions, (error, info) => {
+//       if (error) {
+//           return console.log(error);
+//       }
+//       console.log('Message sent: %s', info.messageId);
+//       console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+//       res.redirect("/");
+//   });
+//   });
+
 app.post('/send', (req, res) => {
   const output = `
     <p>You have a new contact request</p>
@@ -87,22 +132,19 @@ app.post('/send', (req, res) => {
 
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
-    host: 'mail.portfoliosthy2020.com', 
+    host: 'smtp.gmail.com',
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
-      user: 'portfoliosthy2020@gmail.com',
-      pass: 'portfolio2020'  // generated ethereal password
-    },
-    tls:{
-      rejectUnauthorized:false 
+        user: 'shanusasanu@gmail.com', // generated ethereal user
+        pass: 'Shanu123416'  // generated ethereal password
     }
   });
 
   // setup email data with unicode symbols
   let mailOptions = {
-      from: '"Nodemailer Contact" <portfoliosthy2020@gmail.com>', // sender address
-      to: 'saththiyan97@gmail.com', // list of receivers
+      from: '"Nodemailer Contact" <shanusasanu@gmail.com>', // sender address
+      to: 'dilaxn@gmail.com', // list of receivers
       subject: 'Node Contact Request', // Subject line
       text: 'Hello world?', // plain text body
       html: output // html body
@@ -119,8 +161,6 @@ app.post('/send', (req, res) => {
       res.redirect("/");
   });
   });
-
-
 
 
 
