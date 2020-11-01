49c81580c8aee9d1efc4dd2c9a24f302009e7c8c
sathiyan.A <saththiyan97@gmail.com>
2020-11-01 19:20:12 +0530

    Add

--- app.js
+++ app.js
@@ -170,8 +170,6 @@ app.post('/send', (req, res) => {
 let port =process.env.PORT;
 if(port==null||port==""){port=3000;}
 
-
-
 app.listen(port, function() {
   console.log("Server has started on port successfully.");
 });
